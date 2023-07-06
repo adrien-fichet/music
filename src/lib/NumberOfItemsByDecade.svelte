44b5c362021cc85d98f2540af979cf8c3b693332
Adrien Fichet <adrien.fichet@gmail.com>
2023-07-06 16:57:58 +0200

    Additional stats in NumberOfItemsByDecade

--- src/lib/NumberOfItemsByDecade.svelte
+++ src/lib/NumberOfItemsByDecade.svelte
@@ -3,21 +3,32 @@
 
   let items_by_decade = {}
 
-  $items.forEach(item => {
+  for (const item of $items) {
     let decade = Math.floor(item.year / 10) * 10
+    let listened = !!item.listened
+
     if (decade >= 1950) {
-      if (items_by_decade[decade] == null) {
-        items_by_decade[decade] = 1
+      if (items_by_decade[decade] === undefined) {
+        items_by_decade[decade] = { listened: listened ? 1 : 0, total: 1 }
       } else {
-        items_by_decade[decade] += 1
+        items_by_decade[decade].listened += listened ? 1 : 0
+        items_by_decade[decade].total += 1
       }
     }
-  })
+  }
 </script>
 
-<p>Number of items by decade:</p>
-<ul>
+<p>Number of items listened by decade:</p>
+<table>
+  <tr><th>Decade</th><th>Listened</th><th>Not listened</th><th>Total</th><th>Percentage</th></tr>
   {#each Object.entries(items_by_decade) as [decade, number_of_items]}
-    <li>{decade}s: {number_of_items}</li>
+    <tr>
+      <td>{ decade }s</td>
+      <td>{ number_of_items["listened"] }</td>
+      <td>{ number_of_items["total"] - number_of_items["listened"] }</td>
+      <td>{ number_of_items["total"] }</td>
+      <td>{ Math.floor(number_of_items["listened"] / number_of_items["total"] * 100) }%</td>
+    </tr>
   {/each}
-</ul>
+</table>
+<p></p>
