070337b7c0e9db908a517e7ff8cbd919324cc505
Adrien Fichet <adrien.fichet@gmail.com>
2023-07-19 09:11:58 +0200

    Update data

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -15,5 +15,5 @@
     <li><a href="https://en.wikipedia.org/wiki/List_of_jazz_fusion_musicians">Jazz fusion bands/musicians</a></li>
     <li><a href="https://en.wikipedia.org/wiki/DJ-Kicks">DJ-KiCKS</a></li>
     <li>More Stax, Chess, Daptone & Motown records</li>
-    <li>Songs from the <a href="https://en.wikipedia.org/wiki/National_Recording_Registry">US National Recording Registry</a></li>
+    <li>Songs from the <a href="https://en.wikipedia.org/wiki/National_Recording_Registry">US National Recording Registry</a> and the <a href="https://en.wikipedia.org/wiki/Great_American_Songbook">Great American Songbook</a></li>
 </ul>
