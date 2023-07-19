8bf1df17134cff7702089b5a82abe873774be23b
Adrien Fichet <adrien.fichet@gmail.com>
2023-07-19 16:33:13 +0200

    Ignore unused vars if they begin with _

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,38 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:svelte/recommended"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
     ],
-    "overrides": [
-        {
-            files: ["*.svelte"],
-            parser: "svelte-eslint-parser",
-            parserOptions: {
-                parser: "@typescript-eslint/parser"
-            },
-        },
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-    },
-    "plugins": [
-        "@typescript-eslint",
-    ],
-    "rules": {
-        "@typescript-eslint/ban-ts-comment": "off",
-    },
+  },
 }
