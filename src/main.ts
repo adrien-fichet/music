acf89af56a5ac21db7e4a4b1c641bb67b0db506f
Adrien Fichet <adrien.fichet@gmail.com>
2025-01-06 11:14:46 +0100

    Bump packages

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import './assets/Inter-LightItalic.woff2'
 import './assets/Inter-SemiBold.woff2'
 
 import './assets/app.scss'
+import { mount } from 'svelte'
 import App from './App.svelte'
 
-const app = new App({
+const app = mount(App, {
   target: document.getElementById('app') as HTMLElement,
 })
 
